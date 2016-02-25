c30c17612af7dee55e3063cdeba1e87ac9b8eaaf
Alvin Zhang <mosoft521@gmail.com>
2016-02-25 10:13:14 +0800

    编译错解决02

--- src/main/java/com/gmail/mosoft521/ch01/FloatNumber.java
+++ src/main/java/com/gmail/mosoft521/ch01/FloatNumber.java
@@ -2,12 +2,16 @@ package com.gmail.mosoft521.ch01;
 
 public class FloatNumber {
     public static void main(String[] args) {
-        float a = -0;
+        float a = -5;
         System.out.println(a);
         System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(a)));
     }
 }
 /*
+//0
 0.0
 0
+//-5
+-5.0
+11000000101000000000000000000000
  */
