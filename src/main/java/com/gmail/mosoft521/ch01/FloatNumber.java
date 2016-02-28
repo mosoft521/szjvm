5043c5dacb245cc12fb85e80bbfe28682c6d9580
Alvin Zhang <mosoft521@gmail.com>
2016-02-28 18:51:45 +0800

    ch01 add Guava Strings.padStart()

--- src/main/java/com/gmail/mosoft521/ch01/FloatNumber.java
+++ src/main/java/com/gmail/mosoft521/ch01/FloatNumber.java
@@ -1,16 +1,19 @@
 package com.gmail.mosoft521.ch01;
 
+import com.google.common.base.Strings;
+
 public class FloatNumber {
     public static void main(String[] args) {
-        float a = -5;
-        System.out.println(a);
-        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(a)));
+        float f = -5;
+        System.out.println(f);
+//        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(f)));
+        System.out.println(Strings.padStart(Integer.toBinaryString(Float.floatToRawIntBits(f)),32, '0'));
     }
 }
 /*
-//0
-0.0
-0
+//5
+5.0
+01000000101000000000000000000000
 //-5
 -5.0
 11000000101000000000000000000000
--- src/main/java/com/gmail/mosoft521/ch01/IntNumber.java
+++ src/main/java/com/gmail/mosoft521/ch01/IntNumber.java
@@ -1,17 +1,27 @@
 package com.gmail.mosoft521.ch01;
 
+import com.google.common.base.Strings;
+
 public class IntNumber {
     public static void main(String[] args) {
-        int a = -10;
+        int a = 10;
         for (int i = 0; i < 32; i++) {
             int t = (a & 0x80000000 >>> i) >>> (31 - i);
             System.out.print(t);
         }
         System.out.println();
-        System.out.println(Integer.toBinaryString(a));
+//        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Strings.padStart(Integer.toBinaryString(a),32, '0'));
     }
 }
 /*
+第一次：-10
 11111111111111111111111111110110
 11111111111111111111111111110110
+第二次：10
+00000000000000000000000000001010
+1010
+第三次：10（使用Guava）
+00000000000000000000000000001010
+00000000000000000000000000001010
  */
